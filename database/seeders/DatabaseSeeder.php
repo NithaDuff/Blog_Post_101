9585e45205ac4c62bd84840461a23bd384eb5490
NithaDuff <chalilnitha007@gmail.com>
2022-04-25 01:01:27 +0300

    seeder fix and thumbnail optional

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -53,7 +53,7 @@ class DatabaseSeeder extends Seeder
         $posts = Post::factory(10)->create();
         $tags = Tag::factory(5)->create();
         foreach ($posts as $post) {
-            $post->tags()->sync(Tag::all()->random());
+            $post->tags()->sync(Tag::all()->random(random(1,5)));
         }
         
     }
